84b50f55d657b0225d7ebf9d622c776fda472e55
Jabh <jabh2001@gmail.com>
2024-02-28 14:42:58 -0400

    change contacte router middelware and add contact/label route

--- src/routers/api/contactRouter.ts
+++ src/routers/api/contactRouter.ts
@@ -1,7 +1,21 @@
-import { Router } from "express"
+import { NextFunction, Request, Response, Router } from "express"
 import { deleteContact, getContactById, getContacts, saveNewContact, updateContact } from "../../service/contactService"
 import { ContactType } from "../../types"
 const contactRouter = Router()
+
+const getModelMiddleware = async (req:Request, res:Response, next:NextFunction) => {
+    try {
+
+        const contact = await getContactById(Number(req.params.id))
+        req.contact = contact
+    } catch(e) {
+        return res.status(404).json({ error : 'Contact not found'})
+    }
+    next()
+
+}
+contactRouter.use("/:id", getModelMiddleware)
+contactRouter.use("/:id/labels", getModelMiddleware)
 
 contactRouter.route("/")
     .get(async(req, res) => {
@@ -14,17 +28,20 @@ contactRouter.route("/")
         res.json({ contact })
     })
 
-contactRouter.use("/:id", async (req, res, next)=>{
-    try {
-
-        const contact = await getContactById(Number(req.params.id))
-        req.contact = contact
-    } catch(e) {
-        return res.status(404).json({ error : 'Contact not found'})
-    }
-    next()
-})
 contactRouter.route("/:id")
+    .get(async (req, res) => {
+        res.json({ contact:req.contact })
+    })
+    .put(async (req, res) => {
+        const contact = await updateContact(req.contact, req.body)
+        res.json({ contact })
+    })
+    .delete(async (req, res) => {
+        const contact = await deleteContact(req.contact)
+        res.json({ contact })
+    })
+
+contactRouter.route("/:id/labels")
     .get(async (req, res) => {
         res.json({ contact:req.contact })
     })
