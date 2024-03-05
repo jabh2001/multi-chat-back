3934393d33b8b6090a5019c3373027d1c5790096
Jabh <jabh2001@gmail.com>
2024-03-05 12:54:01 -0400

    add all models

--- src/libs/models.ts
+++ src/libs/models.ts
@@ -38,6 +38,43 @@ const ContactLabelModel = (
     .addColumn("labelId")
 )
 
+const UserModel = (
+    new Model("user")
+    .addColumn(Column.primary("id"))
+    .addColumn("name")
+    .addColumn("email")
+    .addColumn("role")
+)
+
+const InboxModel = (
+    new Model("inbox")
+    .addColumn(Column.primary("id"))
+    .addColumn("name")
+    .addColumn("channelType")
+)
+
+const ConversationModel = (
+    new Model("conversation")
+    .addColumn(Column.primary("id"))
+    .addColumn("accountId")
+    .addColumn("inboxId")
+    .addColumn("senderId")
+    .addColumn("userId")
+)
+
+
+const MessageModel = (
+    new Model("message")
+    .addColumn(Column.primary("id"))
+    .addColumn("conversationId")
+    .addColumn("content")
+    .addColumn("contentType")
+    .addColumn("messageType")
+    .addColumn("private")
+    .addColumn("createdAt")
+)
+
+
 // async function main(){
 //     const result = await ContactModel.query.join(SocialMediaModel, SocialMediaModel.c.contactId).fetchAllQuery()
 //     console.log(result)
@@ -52,4 +89,8 @@ export {
     ContactModel,
     ContactLabelModel,
     SocialMediaModel,
+    UserModel,
+    InboxModel,
+    ConversationModel,
+    MessageModel,
 }
