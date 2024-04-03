8b5d7c886f67094647de13bdf0b504a4273088bd
Jabh <jabh2001@gmail.com>
2024-04-03 10:33:47 -0400

    add blank profile photo

--- src/routers/api/conversationRouter.ts
+++ src/routers/api/conversationRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import { getConversations } from "../../service/conversationService"
+import { getContactAvatarUrl } from "../../service/contactService"
 
 const conversationRouter = Router()
 
@@ -7,8 +8,11 @@ conversationRouter.get("/", async (req, res) => {
         try{
             const conversations = await getConversations(req.query)
             res.json({ conversations:conversations.map((c:any) => {
-                const { conversation, ...rest } = c
-                return { ...conversation, ...rest }
+                const { conversation, contact, ...rest } = c
+                return { ...conversation, contact:{
+                    ...contact,
+                    avatarUrl:getContactAvatarUrl(req, contact.id)
+                }, ...rest }
             }) })
         } catch (e:any){
             return res.status(500).json({ error: e.message })
